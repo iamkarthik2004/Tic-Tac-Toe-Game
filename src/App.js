f13b72c2087f2e4d5d23772f710c40db8601caa2
Karthik Krishnan <141357296+iamkarthik2004@users.noreply.github.com>
2025-03-22 21:03:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TicTacToe from './Components/TicTacToe/Tic-Tac-Toe.jsx';
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
